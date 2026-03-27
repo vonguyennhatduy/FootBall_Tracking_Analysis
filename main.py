5ab5c313f228e5f71b1c4dbf1fe7c1a5fb3efb96
vonguyennhatduy0706@gmail.com <vonguyennhatduy7625@gmail.com>
2026-03-27 20:54:58 +0700

    project in progress

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils import read_video, save_video
+
+
+def main (): 
+    # Read Video 
+    video_frame = read_video('input_videos/08fd33_4.mp4')
+    
+    # Save Video 
+    save_video(video_frame, 'output_videos/output_video.avi')
+
+    
+
+
+if __name__ == "__main__":
+    main()
